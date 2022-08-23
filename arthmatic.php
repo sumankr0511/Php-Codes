b38f66570a8ad70a0332a7f486c3170520f8cb98
sumankr0511 <sumanankan4@gmail.com>
2022-08-23 09:16:25 +0530

    second commit

---
+++ arthmatic.php
@@ -0,0 +1,28 @@
+<?php
+// + - * / % 
+$a=20;
+$b=5;
+
+//add
+ $c=$a+$b;
+ echo "The Sum of no. is : $c </br>"; 
+
+//sub
+ $c=$a-$b;
+ echo "$c </br>";
+
+ //mul
+ $c=$a*$b;
+ echo "$c </br>";
+
+ //div
+ $c=$a/$b;
+ echo "$c </br>";
+
+ //mode
+
+ $c=$a%$b;
+ echo $c;
+
+
+?>
